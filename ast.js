a4a4d27d2375bc7df212e64a4cb0886b59cbb1cf
James Gunn <jgunn987@gmail.com>
2019-04-06 22:25:55 +0100

    backtracking

--- ast.js
+++ ast.js
@@ -6,11 +6,14 @@ const treeify = require('treeify');
 
 function TokenIterator(tokens) {
   let cursor = 0;
+  let length = tokens.length;
   return {
     current: () => tokens[cursor],
     peek: () => tokens[cursor+1],
     next: () => tokens[++cursor],
-    prev: () => tokens[(--cursor) || 0]
+    prev: () => tokens[(--cursor) || 0],
+    index: () => cursor,
+    seek: (i) => cursor = i
   };
 }
 
@@ -40,7 +43,17 @@ const types = {
   Terminator: Token('Terminator'),
 };
 
+function loadLexicon(lexicon) {
+  return _.fromPairs(_.toPairs(lexicon).map(pair =>
+    [pair[0], types[pair[1].type]({ id: pair[0] })]));
+}
+
+function lexer(lexicon) {
+  return input => TokenIterator(input.split(' ').map(p => lexicon[p]));
+}
 const SentenceNode = Node('Sentence');
+const DeclarativeSentenceNode = Node('DeclarativeSentence');
+const ImperativeSentenceNode = Node('ImperativeSentence');
 const NounPhraseNode = Node('NounPhrase');
 const PrePositionPhraseNode = Node('PrePositionPhrase');
 const VerbPhraseNode = Node('VerbPhrase');
@@ -48,11 +61,42 @@ const VerbPhraseNode = Node('VerbPhrase');
 function parse(iter) {
 
   function Sentence() {
+    const current = iter.current();
+    let sentence;
+    switch(current.type) {
+      case 'PrePosition':
+        return;
+      case 'Determiner':
+      case 'ProNoun':
+      case 'ConcreteNoun':
+      case 'AbstractNoun':
+      case 'ProperNoun':
+        sentence = DeclarativeSentence();
+        if(!sentence) return;
+        return SentenceNode({ left: sentence });
+      case 'Verb':
+        sentence = ImperativeSentence();
+        if(!sentence) return;
+        return SentenceNode({ left: sentence });
+    }
+  }
+
+  function DeclarativeSentence() {
     const subject = NounPhrase();
+    if(!subject) return;
     iter.next();
     const predicate = VerbPhrase();
-    return SentenceNode({
+    if(!predicate) return;
+    return DeclarativeSentenceNode({
       left: subject, right: predicate
+    });
+  }
+
+  function ImperativeSentence() {
+    const predicate = VerbPhrase();
+    if(!predicate) return;
+    return ImperativeSentenceNode({
+      left: predicate
     });
   }
 
@@ -80,13 +124,17 @@ function parse(iter) {
   }
 
   function NounPhraseRecurse(node) {
+    let right;
     switch(iter.peek().type) {
       case 'PrePosition':
         iter.next();
+        right = PrePositionPhrase();
+        if(!right) return;
         return NounPhraseRecurse(NounPhraseNode({
-          left: node, right: PrePositionPhrase()
+          left: node, right
         }));
       case 'Conjunction':
+        const start = iter.index();
         const op = iter.next();
         iter.next();
         switch(iter.current().type) {
@@ -95,12 +143,13 @@ function parse(iter) {
           case 'ConcreteNoun':
           case 'AbstractNoun':
           case 'ProperNoun':
+            right = NounPhrase();
+            if(!right) return;
             return NounPhraseRecurse(NounPhraseNode({
-              left: node, op, right: NounPhrase()
+              left: node, op, right
             }));
         }
-        iter.prev();
-        iter.prev();
+        iter.seek(start);
         return node;
       case 'Determiner':
       case 'ProNoun':
@@ -108,30 +157,37 @@ function parse(iter) {
       case 'AbstractNoun':
       case 'ProperNoun':
         iter.next();
+        right = NounPhrase();
+        if(!right) return;
         return NounPhraseNode({
-          left: node, right: NounPhrase()
+          left: node, right
         });
     }
     return node;
   }
 
   function VerbPhrase() {
+    let right;
     const current = iter.current();
     switch(current.type) {
       case 'Verb':
         switch(iter.peek().type) {
           case 'PrePosition':
             iter.next();
+            right = PrePositionPhrase();
+            if(!right) return;
             return VerbPhraseRecurse(VerbPhraseNode({
-              left: current, right: PrePositionPhrase()
+              left: current, right
             }));
           case 'ProNoun':
           case 'ConcreteNoun':
           case 'AbstractNoun':
           case 'ProperNoun':
             iter.next();
+            right = NounPhrase();
+            if(!right) return;
             return VerbPhraseRecurse(VerbPhraseNode({
-              left: current, right: NounPhrase()
+              left: current, right
             }));
         }
         return VerbPhraseRecurse(VerbPhraseNode({
@@ -141,29 +197,35 @@ function parse(iter) {
   }
 
   function VerbPhraseRecurse(node) {
+    let right;
     switch(iter.peek().type) {
       case 'Conjunction':
+        const start = iter.index();
         const op = iter.next();
         iter.next();
         switch(iter.current().type) {
           case 'Verb':
+            right = VerbPhrase();
+            if(!right) return;
             return VerbPhraseRecurse(VerbPhraseNode({
-              left: node, op, right: VerbPhrase()
+              left: node, op, right
             }));
         }
-        iter.prev();
-        iter.prev();
+        iter.seek(start);
         return node;
       case 'Verb':
         iter.next();
+        right = VerbPhrase();
+        if(!right) return;
         return VerbPhraseNode({
-          left: node, right: VerbPhrase()
+          left: node, right
         });
     }
     return node;
   }
 
   function PrePositionPhrase() {
+    let right;
     const current = iter.current();
     switch(current.type) {
       case 'PrePosition':
@@ -171,15 +233,19 @@ function parse(iter) {
           case 'Conjunction':
             const op = iter.next();
             iter.next();
+            right = PrePositionPhrase();
+            if(!right) return;
             return PrePositionPhraseRecurse(
               PrePositionPhraseNode({
-                left: current, op, right: PrePositionPhrase()
+                left: current, op, right
               }));
           case 'Verb':
             iter.next();
+            right = PrePositionPhrase();
+            if(!right) return;
             return PrePositionPhraseRecurse(
               PrePositionPhraseNode({
-                left: current, right: VerbPhrase()
+                left: current, right
               }));
           case 'Determiner':
           case 'ProNoun':
@@ -187,9 +253,11 @@ function parse(iter) {
           case 'AbstractNoun':
           case 'ProperNoun':
             iter.next();
+            right = NounPhrase();
+            if(!right) return;
             return PrePositionPhraseRecurse(
               PrePositionPhraseNode({
-                left: current, right: NounPhrase()
+                left: current, right
               }));
         }
     }
@@ -199,8 +267,10 @@ function parse(iter) {
     switch(iter.peek().type) {
       case 'PrePosition':
         iter.next();
+        right = PrePositionPhrase();
+        if(!right) return;
         return PrePositionPhraseNode({
-          left: node, right: PrePositionPhrase()
+          left: node, right
         });
     }
     return node;
@@ -209,12 +279,11 @@ function parse(iter) {
   return Sentence();
 }
 
-const dict = _.fromPairs(_.toPairs(lexicon).map(pair =>
-  [pair[0], types[pair[1].type]({ id: pair[0] })]));
 
 runTests();
 
 function runTests() {
+  const l = lexer(loadLexicon(lexicon));
   [
     /*
     //'james .',
@@ -235,7 +304,11 @@ function runTests() {
     'john and james and peter .',
     'a car and john and sue peter drive and drink to the shop and smoke weed .',
     'i go to and from death and die .',
-    'i go to and from the shop and drink beer .'
+    'i go to and from the shop and drink beer .',
+    'james go to the shop to drink beer , peter go to the shop to drink beer , john go to the shop to drink beer .',
+    'the go go shop .',
+    'go to a shop .'
+
 /*
     'james , john and peter drink beer from thursday , to tuesday .',
     'go from drink to drive in october , december .',
@@ -247,7 +320,7 @@ function runTests() {
     'to drink go to james .',
 */
   ].forEach(string => {
-    const parsed = parse(TokenIterator(string.split(' ').map(p => dict[p])));
+    const parsed = parse(l(string));
     console.log(treeify.asTree(parsed, true));
     //console.log(JSON.stringify(parsed, null, 2));
   });
